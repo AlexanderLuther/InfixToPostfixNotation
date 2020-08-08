1d39bfdba9ee6b2ad5677f22877388541648daf7
AlexanderLuther <alexanderluther08@gmail.com>
2020-08-08 12:00:36 -0600

    Inicio del proyecto

---
+++ src/main/java/com/hluther/infixtopostfixnotation/InfixToPostfixNotation.java
@@ -0,0 +1,15 @@
+package com.hluther.infixtopostfixnotation;
+
+import com.hluther.gui.PrincipalFrame;
+/**
+ *
+ * @author helmuth
+ */
+public class InfixToPostfixNotation {
+    
+    public static void main(String[] args){
+        PrincipalFrame frame = new PrincipalFrame();
+        frame.setVisible(true);
+    }
+    
+}
